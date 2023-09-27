b6d346623d3e953dd9754a8011a424dca0771731
murphydhee <murphydhee@gmail.com>
2023-09-27 08:24:32 +0100

    page build

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Header from "./components/Header";
 import "./App.css";
-// import Home from "./components/Home";
+import Home from "./components/Home";
 // import Detail from "./components/Detail";
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Login />} />
-          {/* <Route path="/home" element={<Home />} /> */}
+          <Route path="/home" element={<Home />} />
           {/* <Route path="/detail/:id" element={<Detail />} /> */}
         </Routes>
       </Router>
